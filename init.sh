7b8fe7b5b8a6663923a0163c5e16ff72ffb3f70d
Grazfather <grazfather@gmail.com>
2018-08-01 20:50:35 -0700

    init: Don't backup if not needed

--- init.sh
+++ init.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 set -e
 
-dir=$(dirname "$0")
+DIR="$(cd "$(dirname "$0")" ; pwd -P)"
 backup=$HOME/dotfiles.bak
 
+function status {
+    echo "[*]" $@
+}
 
 # Setup symlinks
-for file in $dir/*; do
+for file in $DIR/*; do
     filename=$(basename $file)
-    echo $file
     if [[ "$filename" != "$(basename $0)" ]]; then
         if [ -e $HOME/.$filename ]; then
-            mkdir -p $backup
-            # Move existing dotfile to $backup
-            mv $HOME/.$filename $backup/
+            if ! [ -h $HOME/.$filename ]; then
+                mkdir -p $backup
+                # Move existing dotfile to $backup
+                mv $HOME/.$filename $backup/
+            else
+                status .$filename is already symlinked
+                continue
+            fi
         fi
-        echo "Creating link for .$filename"
-        ln -s $dir/$filename $HOME/.$filename
+        status "Creating link for .$filename"
+        ln -s $DIR/$filename $HOME/.$filename
     fi
 done
 
 # Pull in submodules
+pushd $DIR >/dev/null
 git submodule init && git submodule update
+popd >/dev/null
 
 # Install vim plugins
 vim +'PlugInstall --sync' +qa
@@ -30,10 +39,14 @@ vim +'PlugInstall --sync' +qa
 if ! grep -q bash_aliases $HOME/.bashrc; then
     echo "# Set up aliases" >> $HOME/.bashrc
     echo "[ -f \$HOME/.bash_aliases ] && source \$HOME/.bash_aliases" >> $HOME/.bashrc
+else
+    status ".bash_aliases already in .bashrc"
 fi
 
 # Install git aliases
 if ! grep -q gitaliases $HOME/.gitconfig 2>/dev/null; then
     echo "[include]" >> $HOME/.gitconfig
     echo "	path = ~/.gitaliases" >> $HOME/.gitconfig
+else
+    status ".gitaliases already in .gitconfig"
 fi
