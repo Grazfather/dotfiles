87d526cd20cba7b3285a4f33c7c0679f3a3e56b7
Grazfather <grazfather@gmail.com>
2024-04-05 09:01:21 -0400

    Wezterm: Add binding to make panes/tabs work like tmux

--- wezterm.lua
+++ wezterm.lua
@@ -10,8 +10,10 @@ config.color_scheme = 'N0tch2k'
 -- Key bindings
 config.leader = { key = 'i', mods = "CMD|CTRL|OPT" }
 config.keys = {
+  -- CTRL-SHIFT-L: Debug overlay (logging + REPL)
   -- CMD-R: Reload config
   -- CMD-F: Search
+  -- Use `action = wezterm.action_callback(func)` to assign callback
   {
     key = 's',
     mods = 'LEADER',
@@ -22,10 +24,14 @@ config.keys = {
     mods = 'LEADER',
     action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
   },
-  { key = 'h', mods = 'LEADER', action = act.ActivatePaneDirection 'Left' },
-  { key = 'l', mods = 'LEADER', action = act.ActivatePaneDirection 'Right' },
-  { key = 'k', mods = 'LEADER', action = act.ActivatePaneDirection 'Up' },
-  { key = 'j', mods = 'LEADER', action = act.ActivatePaneDirection 'Down' },
+  { key = 'h', mods = 'LEADER', action = act.ActivatePaneDirection('Left') },
+  { key = 'l', mods = 'LEADER', action = act.ActivatePaneDirection('Right') },
+  { key = 'k', mods = 'LEADER', action = act.ActivatePaneDirection('Up') },
+  { key = 'j', mods = 'LEADER', action = act.ActivatePaneDirection('Down') },
+  { key = 'n', mods = 'LEADER', action = act.ActivateTabRelative(1) },
+  { key = 'p', mods = 'LEADER', action = act.ActivateTabRelative(-1) },
+  { key = 'x', mods = 'LEADER', action = act.CloseCurrentPane({ confirm = true })},
+  { key = 'z', mods = 'LEADER', action = act.TogglePaneZoomState},
 }
 
 return config
