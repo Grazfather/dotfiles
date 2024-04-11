af60ba324aa712a39fd6fec339e8923a72bc0c1d
Grazfather <grazfather@gmail.com>
2024-04-11 09:19:20 -0400

    Wezterm: Change leader and add more bindings

--- wezterm.lua
+++ wezterm.lua
@@ -8,30 +8,24 @@ config.font = wezterm.font('Operator Mono SSm Lig')
 config.color_scheme = 'N0tch2k'
 
 -- Key bindings
-config.leader = { key = 'i', mods = "CMD|CTRL|OPT" }
+config.leader = { key = ',', mods = 'CTRL' }
 config.keys = {
   -- CTRL-SHIFT-L: Debug overlay (logging + REPL)
   -- CMD-R: Reload config
   -- CMD-F: Search
   -- Use `action = wezterm.action_callback(func)` to assign callback
-  {
-    key = 's',
-    mods = 'LEADER',
-    action = act.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'v',
-    mods = 'LEADER',
-    action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
+  { key = ':', mods = 'LEADER', action = act.ActivateCommandPalette },
+  { key = 's', mods = 'LEADER', action = act.SplitVertical },
+  { key = 'v', mods = 'LEADER', action = act.SplitHorizontal },
   { key = 'h', mods = 'LEADER', action = act.ActivatePaneDirection('Left') },
   { key = 'l', mods = 'LEADER', action = act.ActivatePaneDirection('Right') },
   { key = 'k', mods = 'LEADER', action = act.ActivatePaneDirection('Up') },
   { key = 'j', mods = 'LEADER', action = act.ActivatePaneDirection('Down') },
   { key = 'n', mods = 'LEADER', action = act.ActivateTabRelative(1) },
   { key = 'p', mods = 'LEADER', action = act.ActivateTabRelative(-1) },
-  { key = 'x', mods = 'LEADER', action = act.CloseCurrentPane({ confirm = true })},
-  { key = 'z', mods = 'LEADER', action = act.TogglePaneZoomState},
+  { key = 'x', mods = 'LEADER', action = act.CloseCurrentPane({ confirm = true }) },
+  { key = 'z', mods = 'LEADER', action = act.TogglePaneZoomState },
+  { key = 'a', mods = 'LEADER|CTRL', action = act.ActivateLastTab },
 }
 
 return config
